0d2a5cc290f814bb7326a041f6b14a475c08c6c4
Andrew <andrew.kenny@forwessun.com>
2020-09-14 16:08:43 +0100

    removed autopep8 from create_exe, added the pyinstaller settings.

--- create_exe.bat
+++ create_exe.bat
@@ -2,31 +2,31 @@
 @echo off
 
 :: automatically format
-autopep8 --in-place "./fixture_processor/__init__.py"
-autopep8 --in-place "./fixture_processor/__main__.py"
-autopep8 --in-place "./fixture_processor/file_operations.py"
-autopep8 --in-place "./fixture_processor/fixture_processor_form.py"
-autopep8 --in-place "./fixture_processor/helper_functions.py"
-
-
-
-autopep8 --in-place "./fixture_processor/fixture_functions/extract_wires.py"
-autopep8 --in-place "./fixture_processor/fixture_functions/fixture_input.py"
-autopep8 --in-place "./fixture_processor/fixture_functions/fixture_maths.py"
-autopep8 --in-place "./fixture_processor/fixture_functions/fixture_modifications.py"
-autopep8 --in-place "./fixture_processor/fixture_functions/fixture_output.py"
-autopep8 --in-place "./fixture_processor/fixture_functions/fixture_processing.py"
-autopep8 --in-place "./fixture_processor/fixture_functions/output_data.py"
-
-
-autopep8 --in-place "./fixture_processor/options_lib/__init__.py"
-autopep8 --in-place "./fixture_processor/options_lib/options_functions.py"
-autopep8 --in-place "./fixture_processor/options_lib/program_options.py"
-autopep8 --in-place "./fixture_processor/options_lib/fixture_processing_options.py"
+:: autopep8 --in-place "./fixture_processor/__init__.py"
+:: autopep8 --in-place "./fixture_processor/__main__.py"
+:: autopep8 --in-place "./fixture_processor/file_operations.py"
+:: autopep8 --in-place "./fixture_processor/fixture_processor_form.py"
+:: autopep8 --in-place "./fixture_processor/helper_functions.py"
+:: 
+:: 
+:: 
+:: autopep8 --in-place "./fixture_processor/fixture_functions/extract_wires.py"
+:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_input.py"
+:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_maths.py"
+:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_modifications.py"
+:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_output.py"
+:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_processing.py"
+:: autopep8 --in-place "./fixture_processor/fixture_functions/output_data.py"
+:: 
+:: 
+:: autopep8 --in-place "./fixture_processor/options_lib/__init__.py"
+:: autopep8 --in-place "./fixture_processor/options_lib/options_functions.py"
+:: autopep8 --in-place "./fixture_processor/options_lib/program_options.py"
+:: autopep8 --in-place "./fixture_processor/options_lib/fixture_processing_options.py"
 
 :: check code quality
 pylint fixture_processor > pylint_output.txt
 flake8 fixture_processor > flake8_output.txt
 
 
-# pyinstaller.exe --distpath=.\bin\fixture_processor --onefile  --noupx wires_processor.py
+pyinstaller.exe --distpath=.\bin\fixture_processor --onefile  --noupx wires_processor.py
