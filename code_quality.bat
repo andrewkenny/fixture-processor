9dc05df3d53fbd099107272388f2d35e4c7ad251
Andrew <andrew.kenny@forwessun.com>
2020-09-16 16:03:31 +0100

    move pylint and flake8 to code_quality.bat

---
+++ code_quality.bat
@@ -0,0 +1,6 @@
+
+@echo off
+
+:: check code quality
+pylint fixture_processor > pylint_output.txt
+flake8 fixture_processor > flake8_output.txt
--- create_exe.bat
+++ create_exe.bat
@@ -3,28 +3,7 @@
 
 set InitialPath=%PATH%
 
-:: automatically format
-:: autopep8 --in-place "./fixture_processor/__init__.py"
-:: autopep8 --in-place "./fixture_processor/__main__.py"
-:: autopep8 --in-place "./fixture_processor/file_operations.py"
-:: autopep8 --in-place "./fixture_processor/fixture_processor_form.py"
-:: autopep8 --in-place "./fixture_processor/helper_functions.py"
-:: 
-:: 
-:: 
-:: autopep8 --in-place "./fixture_processor/fixture_functions/extract_wires.py"
-:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_input.py"
-:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_maths.py"
-:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_modifications.py"
-:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_output.py"
-:: autopep8 --in-place "./fixture_processor/fixture_functions/fixture_processing.py"
-:: autopep8 --in-place "./fixture_processor/fixture_functions/output_data.py"
-:: 
-:: 
-:: autopep8 --in-place "./fixture_processor/options_lib/__init__.py"
-:: autopep8 --in-place "./fixture_processor/options_lib/options_functions.py"
-:: autopep8 --in-place "./fixture_processor/options_lib/program_options.py"
-:: autopep8 --in-place "./fixture_processor/options_lib/fixture_processing_options.py"
+
 
 :: check code quality
 ::pylint fixture_processor > pylint_output.txt
