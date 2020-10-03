b1872a8bae1c4a7945bfe40222bc0256a3b5cabd
Andrew <andrew.kenny@forwessun.com>
2020-10-03 13:22:06 +0100

    removed unneeded function.

--- fixture_processor/helper_functions.py
+++ fixture_processor/helper_functions.py
@@ -5,17 +5,4 @@ not specific to fixture processing.
 """
 
 
-def iter_dict_values(dictionary_arg: dict)
 
-"""
-    Very often when iterating through a dictionary,
-    the value is a list or another sort of iterator,
-    and that is then iterated on.
-
-    This creates an unnecessary indentation.
-    """
-
-for key, item_iterable in dictionary_arg.items():
-
-    for item in item_iterable:
-        yield key, item
