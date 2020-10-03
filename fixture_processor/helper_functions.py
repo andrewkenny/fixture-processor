c47fd9c025ba3dda9d0f02228780107cf141fe82
Andrew <andrew.kenny@forwessun.com>
2020-10-03 16:42:28 +0100

    add error message based helper function for error headers.

--- fixture_processor/helper_functions.py
+++ fixture_processor/helper_functions.py
@@ -5,4 +5,19 @@ not specific to fixture processing.
 """
 
 
-
+def error_message_header(filename):
+    """
+    generates the error header, seen at the top
+    of all messagebox errors.
+    """
+    
+    def inner(line_number, raw_line):
+        
+        inner_filename = filename
+        
+        # removing trailing newline from rawline.
+        raw_line = raw_line.rstrip("\r\n")
+        
+        return f"    Error when parsing '{inner_filename}'\n"\
+               f"    {line_number}: {raw_line}\n
+        
