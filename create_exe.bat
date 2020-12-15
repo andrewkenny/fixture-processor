3fa1d073b28112051135711b37e578c4de347e01
Andrew <andrew.kenny@forwessun.com>
2020-12-15 16:23:43 +0000

    fix known issues with create_exe.bat.
    Having to create a 32 bit and a 64 bit exe has been problematic.
    
    The 64 bit was created for performance reasons etc, and the 32 bit
    for compatability.
    
    But due to me not adding the 32 bit version to the path, there were
    some problems everytime I tried to account for this in the batch file.
    
    Now the batch file:
        keeps a backup of the build files
        copies the build files into the folder at the start of the build
        resets the path after every tun.
    
        also, due to the common build files, it tends to run faster.

--- create_exe.bat
+++ create_exe.bat
@@ -1,34 +1,55 @@
 @echo off
+@SETLOCAL
 
 set Processor=%1
 
 set CorrectArg=0
 
-echo %localappdata%
+if [%1]==[] (
+    echo The first Argument must be '32' or '64'
+    exit /B
+
+)
+
+
 
 
 del .\build\wires_processor\* /q  >nul 2>&1
 
 if %Processor% == 64 (
 
+    echo 64 > build/processor_bits
+    xcopy .\build\wires_processor_64 .\build\wires_processor /E >nul 2>&1
     set CorrectArg=1
     
     
     pyinstaller --distpath=.\bin\fixture_processor --onefile  --icon=assets\index.ico --noupx wires_processor.py
-
+    
+    :: make a copy of the build folder for quicker future builds.
+    del .\build\wires_processor_64\* /q  >nul 2>&1
+    
+    xcopy .\build\wires_processor .\build\wires_processor_64 /E >nul 2>&1
+    
 )
 
+
+
 if %Processor% == 32 (
+
+    echo 32 > build/processor_bits
+    xcopy .\build\wires_processor_32 .\build\wires_processor /E >nul 2>&1
     set CorrectArg=1
 
-
-    set InitialPath=%PATH%
-    SET PATH=%localappdata%\Programs\Python\Python37-32
-    SET PATH=%PATH%;%localappdata%\Programs\Python\Python37-32\Scripts
+    SET "%localappdata%\Programs\Python\Python37-32"
+    SET "PATH=%PATH%;%localappdata%\Programs\Python\Python37-32\Scripts"
     
     "%localappdata%\Programs\Python\Python37-32\Scripts\pyinstaller.exe" --distpath=.\bin\fixture_processor_x86 --onefile  --icon=assets\index.ico --noupx wires_processor.py
     
-    set PATH=%InitialPath%
+    
+    del .\build\wires_processor_32\* /q  >nul 2>&1
+    
+    xcopy .\build\wires_processor .\build\wires_processor_32 /E >nul 2>&1
+    
 )
 
 if %CorrectArg% == 0 (
